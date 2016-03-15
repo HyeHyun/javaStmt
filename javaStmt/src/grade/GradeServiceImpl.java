1865d7b9e82e22cf3ef947f9ffca9e7ba770de36
USER <hkim9414@naver.com>
2016-03-15 09:41:13 +0900

    ..

--- javaStmt/src/grade/GradeServiceImpl.java
+++ javaStmt/src/grade/GradeServiceImpl.java
@@ -1,5 +1,52 @@
 package grade;
 
-public class GradeServiceImpl {
+public class GradeServiceImpl implements GradeService {
 
+	@Override
+	public void input() {
+		// 성적표 등록
+		
+	}
+
+	@Override
+	public void getList() {
+		// 성적표 리스트 출력
+		
+	}
+
+	@Override
+	public void getGradeByHak() {
+		// 성적표 조회(학번)
+		
+	}
+
+	@Override
+	public void getGradesByName() {
+		// 성적표 조회(이름)
+		
+	}
+
+	@Override
+	public void getCount() {
+		// 카운트 조회
+		
+	}
+
+	@Override
+	public void getCountByName() {
+		// 이름조회시 카운트 조회
+		
+	}
+
+	@Override
+	public void update() {
+		// 성적표 수정
+		
+	}
+
+	@Override
+	public void delete() {
+		// 성적표 삭제
+		
+	}
 }
