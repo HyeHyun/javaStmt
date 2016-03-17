c42c0bd5fb1fa1d57f64f3e6bb3187bee442a951
USER <hkim9414@naver.com>
2016-03-17 14:58:11 +0900

    은닉화 switch/case

--- javaStmt/src/switchcase/MonthEndDay.java
+++ javaStmt/src/switchcase/MonthEndDay.java
@@ -1,38 +1,38 @@
 package switchcase;
 
-import java.util.Scanner;
 import ifelse.LeapYear;
 
-/**
- * @file : MonthEndDay.java
- * @author KIM HYEHYEON
- * @date 2016. 3. 11.
- * @story 해당 월의 말일을 출력함
- * 예를 들면 1월이면 31일 출력
- */
 public class MonthEndDay {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("년도, 월을 입력하세요 ");
-		int year = scanner.nextInt(), month = scanner.nextInt(), endDay = 0;
-		String yearStr = LeapYear.yearStr(year);
+	String yearStr, result;
+	
+	public void setMonthEndDay(int year, int month) {
+		yearStr = LeapYear.yearStr(year);
 		
 		switch (month) {
-		case 1:case 3:case 5:case 7:case 8:case 10:case 12: endDay = 31;break;
-		case 4:case 6:case 9:case 11: endDay = 30;break;
+		case 1:case 3:case 5:case 7:case 8:case 10:case 12:
+			result = year + "년 " + month + "월은 31일이 말일입니다.";
+			break;
+		case 4:case 6:case 9:case 11:
+			result = year + "년 " + month + "월은 30일이 말일입니다.";
+			break;
 		
 		case 2:
 			if (yearStr == "윤년") {
-				endDay = 29;break;
+				result = year + "년 " + month + "월은 29일이 말일입니다.";
+				break;
 			} else {
-				endDay = 28;break;
+				result = year + "년 " + month + "월은 28일이 말일입니다.";
+				break;
 			}
 		
 		default:
-			System.out.println("잘못된 값");
+			result = "잘못된 값";
 			return;
 		}
-		
-		System.out.println(year + "년 " + month + "월은 " + endDay + "일이 말일입니다.");
+	}
+	
+	@Override
+	public String toString() {
+		return result;
 	}
 }
--- javaStmt/src/switchcase/Pass.java
+++ javaStmt/src/switchcase/Pass.java
@@ -1,30 +1,11 @@
 package switchcase;
 
-import java.util.Scanner;
-
-/**
- * @file : Pass.java
- * @author KIM HYEHYEON
- * @date 2016. 3. 11.
- * @story 과목 점수를 입력하면 총점과 평균이 나오고
- * 이를 통해 합격 여부를 통지하는 프로그램 (스위치케이스 처리)
- * [결과]
- * **************************************************
- * *학생		자바	JSP	SQL	스프링	총점	평균		합격여부	
- * *-------------------------------------------------
- * *홍길동	80	40	60	70		250	62.5	합격		
- * **************************************************
- * 합격 여부는 60 이상이면 합격
- */
 public class Pass {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("이름, 자바, JSP, SQL, 스프링 점수를 입력하세요 ");
-		String name = scanner.next();
-		int java = scanner.nextInt(), jsp = scanner.nextInt(), sql = scanner.nextInt(), spring = scanner.nextInt(), sum = 0;
-		double avg = 0.0;
-		String result = "";
-		
+	int sum, java, jsp, sql, spring;
+	double avg;
+	String result, name;
+	
+	public void setData(String name, int java, int jsp, int sql, int spring) {
 		sum = java + jsp + sql + spring;
 		avg = (double)sum / 4;
 		
@@ -33,10 +14,15 @@ public class Pass {
 		default: result = "불합격";break;
 		}
 		
-		System.out.println("******************************************************************");
-		System.out.println("*학생\t자바\tJSP\tSQL\t스프링\t총점\t평균\t합격여부");
-		System.out.println("*-----------------------------------------------------------------");
-		System.out.println("*" + name + "\t" + java + "\t" + jsp + "\t" + sql + "\t" + spring + "\t" + sum + "\t" + avg + "\t" + result);
-		System.out.println("******************************************************************");
+		this.name = name;
+		this.java = java;
+		this.jsp = jsp;
+		this.sql = sql;
+		this.spring = spring;
+	}
+
+	@Override
+	public String toString() {
+		return name + "\t" + java + "\t" + jsp + "\t" + sql + "\t" + spring + "\t" + sum + "\t" + avg + "\t" + result;
 	}
 }
